8789db96afc25f5cda18c91ee1eb45deb7768a61
Corina Wong <hi@corinawong.com>
2017-10-12 14:59:12 +0000

    Create registration page and GET /register endpoint

--- express_server.js
+++ express_server.js
@@ -109,6 +109,12 @@ app.post('/logout', (req, res) => {
   res.redirect('/urls');
 });
 
+// Registration page
+
+app.get('/register', (req, res) => {
+  res.render('register')
+})
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
